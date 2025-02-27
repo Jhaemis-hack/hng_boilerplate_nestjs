7e9859a0b5db9c1cc98002fc7001448c5315adb7
timiwritescode <odebodezion@gmail.com>
2025-02-27 12:47:28 +0100

    feat: add email event to user registration to send welcome email upon user registration

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -84,6 +84,14 @@ export default class AuthenticationService {
       oranisations: userOranisations,
     };
 
+    // send welcome mail
+    await this.emailService.sendUserConfirmationMail(
+      user.email,
+      user.first_name,
+      `${process.env.FRONTEND_URL}/confirm-email`,
+      token
+    );
+
     return {
       message: SYS_MSG.USER_CREATED_SUCCESSFULLY,
       access_token,
